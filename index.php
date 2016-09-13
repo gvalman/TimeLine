2cf7c5e4a305f1ef1e4a1a95c954084853a8e9c1
gvalman <germanj@ibest.com.br>
2016-09-13 18:32:22 -0300

    Preparando os resources e implantando os CDN

--- index.php
+++ index.php
@@ -6,12 +6,25 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>TIMELINE</title>
+        <link rel="shortcut icon" href="resources/img/clock.png"/>
+
+        <!--CSS para as Font Awesome icons-->
+        <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+        <!--CSS para o framework Bootstrap-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        
+        <link rel="stylesheet" href="resources/css/index.css">
     </head>
     <body>
         <?php
-        // put your code here
+        echo "OK";
         ?>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="resources/js/index.js"></script>
     </body>
 </html>
